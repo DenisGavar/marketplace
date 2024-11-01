ac0a997400f504e853360381a85de7fe978cd3a2
DenisGavar <tskhelp@gmail.com>
2024-11-01 15:50:56 +0200

    deleted unnecessary comments

--- src/repositories/user.js
+++ src/repositories/user.js
@@ -36,75 +36,7 @@ class UserRepository {
         resolve(row);
       });
     });
-  } 
-
-  /*
-  async getAll() {
-    const op = "repositories.category.getAll";
-    const message = { op: op };
-    this.logger.info("", message);
-
-    return new Promise((resolve, reject) => {
-      const query = `SELECT * FROM categories`;
-      this.db.all(query, [], function (err, rows) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(rows);
-      });
-    });
   }
-
-  async getById(id) {
-    const op = "repositories.category.getById";
-    const message = { op: op, id: id };
-    this.logger.info("", message);
-
-    return new Promise((resolve, reject) => {
-      const query = `SELECT * FROM categories WHERE id = ?`;
-      this.db.get(query, [id], function (err, row) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(row);
-      });
-    });
-  }
-
-  async update(id, data) {
-    const op = "repositories.category.update";
-    const message = { op: op, id: id };
-    this.logger.info("", message);
-
-    const { name } = data;
-
-    return new Promise((resolve, reject) => {
-      const query = `UPDATE categories SET name = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?`;
-      this.db.run(query, [name, id], function (err) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(this.changes);
-      });
-    });
-  }
-
-  async delete(id) {
-    const op = "repositories.category.delete";
-    const message = { op: op, id: id };
-    this.logger.info("", message);
-
-    return new Promise((resolve, reject) => {
-      const query = `DELETE FROM categories WHERE id = ?`;
-      this.db.run(query, [id], function (err) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(this.changes);
-      });
-    });
-  }
-    */
 }
 
 module.exports = UserRepository;
--- src/services/user.js
+++ src/services/user.js
@@ -38,44 +38,6 @@ class UserService {
 
     return { name, token };
   }
-
-  /*
-    async getAll() {
-      const op = "services.category.getAll";
-      const message = { op: op };
-      this.logger.info("", message);
-  
-      const categories = await this.categoryRepository.getAll();
-      return categories;
-    }
-  
-    async getById(id) {
-      const op = "services.category.getById";
-      const message = { op: op, id: id };
-      this.logger.info("", message);
-  
-      const category = await this.categoryRepository.getById(id);
-      return category;
-    }
-  
-    async update(id, data) {
-      const op = "services.category.update";
-      const message = { op: op, id: id };
-      this.logger.info("", message);
-  
-      const category = await this.categoryRepository.update(id, data);
-      return category;
-    }
-  
-    async delete(id) {
-      const op = "services.category.delete";
-      const message = { op: op, id: id };
-      this.logger.info("", message);
-  
-      const category = await this.categoryRepository.delete(id);
-      return category;
-    }
-    */
 }
 
 module.exports = UserService;
